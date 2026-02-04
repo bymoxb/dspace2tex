95865b1d9e6050446170c0687c746113334bb5a3
Luis Fernando Illapa <moxb@outlook.es>
2026-02-04 17:22:42 -0500

    add main functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useActionState } from "react";
+import { convertDspaceToBibtex } from "./actions/actions";
+import Detail from "./componentes/detail";
+import Error from "./componentes/error";
+import Footer from "./componentes/footer";
+import Input from "./componentes/input";
+import { useBackSlash } from "./hooks/use-shortcuts";
 
 export default function Home() {
+
+  const [state, action, pending] = useActionState(convertDspaceToBibtex, null);
+
+  const [searchInputRef, _, setIsFocused] = useBackSlash();
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <main className="container mx-auto grow">
+        <h1 className="text-center font-semibold text-6xl my-4">DSpace2Tex</h1>
+        <form action={action}
+          className="flex flex-col sm:flex-row gap-1 sm:gap-4 items-center"
+        >
+          <Input
+            id="url"
+            type="url"
+            title="DSpace URI/URL"
+            customRef={searchInputRef}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            placeholder="https://repo.edu/handle/1/1"
+            required
+          />
+          <button
+            type="submit"
+            disabled={pending}
+            className={"border rounded-2xl px-2 py-1 cursor-pointer min-w-36" + (pending ? " cursor-progress" : "")}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+            {pending ? "Convert..." : 'Convert'}
+          </button>
+        </form>
+
+        <Error hasError={state?.ok} message={state?.error} />
+
+        <section className="mt-4 flex flex-col gap-4">
+          <Detail
+            open
+            title="BibTeX"
+            value={state?.bibtex ?? ""}
+            rows={8}
+          />
+
+          <Detail
+            title="Ver metadatos extraídos"
+            value={state?.meta ? JSON.stringify(state?.meta, null, 4) : null}
+            rows={15}
+          />
+        </section>
       </main>
-    </div>
+
+      <Footer />
+    </> 
   );
 }
