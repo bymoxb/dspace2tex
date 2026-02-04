a393cf3d8224f5147782d42b24f7967174500c25
Luis Fernando Illapa <moxb@outlook.es>
2026-02-04 18:17:05 -0500

    update footer

--- src/app/componentes/footer.tsx
+++ src/app/componentes/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
             <div className="container mx-auto text-center">
                 <p className="mt-2">
                     Open source project on <a
-                        href="https://github.com/bymoxb/dspace2bibtex"
+                        href="https://github.com/bymoxb/dspace2tex"
                         target="_blank"
                         className="underline text-blue-500"
                         rel="noopener noreferrer">GitHub</a>
